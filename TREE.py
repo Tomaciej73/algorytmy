138c4d2314e9af6f658a9ce020e5ddf091a5ff84
Tomaciej73 <92037535+Tomaciej73@users.noreply.github.com>
2021-12-01 22:42:58 +0100

    Add files via upload

---
+++ TREE.py
@@ -0,0 +1,82 @@
+from typing import *
+
+class TreeNode:
+    value: Any
+    children: List['TreeNode']
+
+    def __init__(self, value):
+        self.value = value
+        self.children = []
+        self.parent = None
+
+    def is_leaf(self) -> bool:
+        if len(self.children) == 0:
+            return True
+        return False
+
+    def add(self, child: 'TreeNode') -> None:
+        child.parent = self
+        self.children.append(child)
+
+    def for_each_deep_first(self, visit: Callable[['TreeNode'], None]) -> None:
+        visit(self)
+
+        for child in self.children:
+            self.for_each_deep_first(visit(child))
+
+    def for_each_level_order(self, visit: Callable[['TreeNode'], None]) -> None:
+        visit(self)
+
+        fifo = self.children
+        while len(fifo):
+            visit(fifo[0])
+            print(fifo[0])
+            fifo += fifo[0].children
+            del fifo[0]
+
+    def get_level(self) -> int:
+        level = 0
+        p = self.parent
+        while p:
+            level += 1
+            p = p.parent
+        return level
+
+    def print_tree(self):
+        spaces = ' ' * self.get_level() * 3
+        if self.parent:
+            prefix = '-'
+        else:
+            prefix = ''
+        print(spaces + prefix + self.value)
+        if self.children:
+            for child in self.children:
+                child.print_tree()
+
+    def printing(self):
+        print(self.value)
+
+    def printing_branch(self):
+        print(self.value)
+        for x in self.children:
+            print(x.value)
+
+drzewko = TreeNode("F")
+b = TreeNode("B")
+a = TreeNode("A")
+d = TreeNode("D")
+c = TreeNode("C")
+e = TreeNode("E")
+g = TreeNode("G")
+i = TreeNode("I")
+h = TreeNode("H")
+drzewko.add(b)
+b.add(a)
+b.add(d)
+d.add(c)
+d.add(e)
+drzewko.add(g)
+g.add(i)
+i.add(h)
+
+drzewko.print_tree()
