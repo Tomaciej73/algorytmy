a3a82b433b9dc1bb51c0ffa7dacafdeee0a07ee9
Tomaciej73 <92037535+Tomaciej73@users.noreply.github.com>
2021-12-08 20:52:01 +0100

    Delete zad04.py

--- zad04.py
+++
@@ -1,9 +0,0 @@
-def foo1(imie, nazwisko):
-    print(imie[0].upper() + "." + nazwisko[0].upper() + nazwisko[1:])
-
-
-def foo2(imie, nazwisko, foo1):
-    foo1(imie, nazwisko)
-
-
-foo2("jan", "kowalski", foo1)
